56be735bd89bbfd2cfba9b711731e2e504edb149
Venesos <114841865+Venesos@users.noreply.github.com>
2026-04-11 23:41:29 -0300

    Add files via upload

---
+++ Classe_Metodo_Abstrato/src/CanalNotificacao.java
@@ -0,0 +1,19 @@
+public abstract class CanalNotificacao {
+    protected String destinatario;
+    protected String mensagem;
+
+    public CanalNotificacao() {
+    }
+
+    public CanalNotificacao(String destinatario, String mensagem) {
+        this.destinatario = destinatario;
+        this.mensagem = mensagem;
+    }
+
+    public void exibirDados() {
+        System.out.println("Destinatário: " + destinatario);
+        System.out.println("Mensagem: " + mensagem);
+    }
+
+    public abstract void enviar();
+}
